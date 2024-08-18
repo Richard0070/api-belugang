9f4cd4dabbdb500399ba1356934c9edd95db54a4
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-08-18 13:22:14 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -30,3 +30,104 @@ def draw_rounded_rectangle(draw, xy, color, border_radius, transparency_percenta
     alpha = int(255 * (1 - transparency_percentage / 100))
     fill_color = (color_rgb[0], color_rgb[1], color_rgb[2], alpha)
     draw.rectangle([x1 + border_radius, y1, x2 - border_radius, y2], fill=fill_color)
+
+# Special character replacements
+special_char_replacements = {
+    "LkU0mY": '?',
+    "erxL8Q": '&',
+    "hnGwq5": '/',
+    "WDigwg": '.',
+    "m3zziu": ':',
+    "VRfbjC": '=',
+    "QjpC4K": '_'
+}
+
+# Number replacements
+number_replacements = {
+    "c9h4n": '1',
+    "yasJb": '2',
+    "QDEMu": '3',
+    "VnBsl": '4',
+    "uji6t": '5',
+    "QafiD": '6',
+    "ip7o3": '7',
+    "ZIxmO": '8',
+    "sAbZG": '9',
+    "Gv2usn": '0'
+}
+
+# Letter replacements (uppercase)
+uppercase_letter_replacements = {
+    "7Vp": 'A',
+    "hrM": 'B',
+    "qaq": 'C',
+    "aGE": 'D',
+    "7Rl": 'E',
+    "Zbc": 'F',
+    "l7j": 'G',
+    "bAh": 'H',
+    "PAq": 'I',
+    "sJu": 'J',
+    "jmd": 'K',
+    "gco": 'L',
+    "8ps": 'M',
+    "0ov": 'N',
+    "lV9": 'O',
+    "7xs": 'P',
+    "Tsp": 'Q',
+    "4iV": 'R',
+    "5y9": 'S',
+    "28N": 'T',
+    "pfN": 'U',
+    "lxF": 'V',
+    "CP5": 'W',
+    "F5L": 'X',
+    "lQ9": 'Y',
+    "SD0": 'Z'
+}
+
+# Letter replacements (lowercase)
+lowercase_letter_replacements = {
+    "uUE": 'a',
+    "z7A": 'b',
+    "F6W": 'c',
+    "MjW": 'd',
+    "RjF": 'e',
+    "3I9": 'f',
+    "qp7": 'g',
+    "SIg": 'h',
+    "4tp": 'i',
+    "wa8": 'j',
+    "iyN": 'k',
+    "R1L": 'l',
+    "n0C": 'm',
+    "KQK": 'n',
+    "yvZ": 'o',
+    "n1t": 'p',
+    "LmD": 'q',
+    "zWj": 'r',
+    "mB1": 's',
+    "wBW": 't'
+}
+
+def decode(encoded_str):
+    decoded_str = encoded_str
+
+    # Decode letter replacements (uppercase)
+    for replacement, letter in uppercase_letter_replacements.items():
+        decoded_str = decoded_str.replace(replacement, letter)
+    
+    # Decode letter replacements (lowercase)
+    for replacement, letter in lowercase_letter_replacements.items():
+        decoded_str = decoded_str.replace(replacement, letter)
+    
+    # Decode number replacements
+    for replacement, number in number_replacements.items():
+        decoded_str = decoded_str.replace(replacement, number)
+    
+    # Decode special character replacements
+    for replacement, char in special_char_replacements.items():
+        decoded_str = decoded_str.replace(replacement, char)
+    pairs = decoded_str.split("&")
+    result_dict = {key_value[0]: key_value[1] for key_value in (pair.split("=", 1) for pair in pairs)}
+    return result_dict
